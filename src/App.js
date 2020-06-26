10c2aebdd6cbf954f116255b107f1c9ede6e4eb2
sarawarnock <sara@sarawarnock.com>
2020-06-26 10:45:20 -0600

    react components

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import PersonalizedHomePage from './personalized-home-page'
 import PastWorkouts from './past-workouts'
 import CreateNewWorkout from './create-new-workout'
 import NewWorkoutCreated from './new-workout-created'
+import Header from './header'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <LandingPage />
         <Navbar />
         <PersonalizedHomePage />
--- src/landing-page.js
+++ src/landing-page.js
@@ -9,6 +9,13 @@ export default class LandingPage extends React.Component {
       <p>Landing Page</p>
       <Login />
       <SignUp />
+      <main className="main">
+            <h1>Workout Creator</h1>
+            <p>Create your own metabolic-conditoning workout</p>
+            <p>Choose how long you want to workout for and the muscle groups you want to work, and we'll do the rest!</p>
+            <button>Sign Up</button>
+            <button>Log In</button>
+        </main>
     </div>
   );
 }
