6c012f327b517ce1fe2c3fc63b1d88782fa4f471
sarawarnock <sara@sarawarnock.com>
2020-08-12 12:35:26 -0600

    .env file update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
     API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT ||
-    "http://localhost:3000/api",
+    "http://localhost:8000/api",
     TOKEN_KEY: 'workout-client-auth-token',
-  }
+}
   
+console.log(process.env.REACT_APP_API_ENDPOINT) 
