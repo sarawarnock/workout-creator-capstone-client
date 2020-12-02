51cb97799ec587e45452b56f02fcc953b1e7240e
Bowie <bowie@thinkful.com>
2020-12-02 09:04:37 -0800

    comment out token-service export

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -1,31 +1,31 @@
-// import config from '../config';
-// import jwtDecode from 'jwt-decode';
+import config from '../config';
+import jwtDecode from 'jwt-decode';
 
-// const TokenService = {
-//   saveAuthToken(token) {
-//     window.localStorage.setItem(config.TOKEN_KEY, token)
-//   },
-//   getAuthToken() {
-//     return window.localStorage.getItem(config.TOKEN_KEY)
-//   },
-//   clearAuthToken() {
-//     window.localStorage.removeItem(config.TOKEN_KEY)
-//   },
-//   hasAuthToken() {
-//     return !!TokenService.getAuthToken()
-//   },
-//   makeBasicAuthToken(userName, password) {
-//     return window.btoa(`${userName}:${password}`)
-//   },
-//   saveUserId(userId) {
-//     return window.sessionStorage.setItem('user_id', userId);
-//   },
-//   saveUserName(first_name) {
-//     return window.sessionStorage.setItem('first_name', first_name)
-//   },
-//   getUserId(user_id) {
-//     return window.sessionStorage.getItem('user_id', user_id)
-//   }
-// };
+const TokenService = {
+  saveAuthToken(token) {
+    window.localStorage.setItem(config.TOKEN_KEY, token)
+  },
+  getAuthToken() {
+    return window.localStorage.getItem(config.TOKEN_KEY)
+  },
+  clearAuthToken() {
+    window.localStorage.removeItem(config.TOKEN_KEY)
+  },
+  hasAuthToken() {
+    return !!TokenService.getAuthToken()
+  },
+  makeBasicAuthToken(userName, password) {
+    return window.btoa(`${userName}:${password}`)
+  },
+  saveUserId(userId) {
+    return window.sessionStorage.setItem('user_id', userId);
+  },
+  saveUserName(first_name) {
+    return window.sessionStorage.setItem('first_name', first_name)
+  },
+  getUserId(user_id) {
+    return window.sessionStorage.getItem('user_id', user_id)
+  }
+};
 
 // export default TokenService;
