450c19eab418e9e195231bd6e2c39c4ad83d84ad
Bowie <lfranckx@thinkful.com>
2020-12-09 12:47:40 -0800

    finish adding defaultProps for workout_id

--- src/view-past-workout-lf.js
+++ src/view-past-workout-lf.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import WorkOutContext from './context';
+import WorkoutApiService from './services/workout-api-service';
 
 export default class ViewPastWorkout extends React.Component {
     static defaultProps = {
@@ -8,7 +9,11 @@ export default class ViewPastWorkout extends React.Component {
 
     static contextType = WorkOutContext;
 
-    componentDidMount()
+    componentDidMount() {
+        const { workout_id } = this.props.match.params;
+        this.context.clearError();
+        // WorkoutApiService
+    }
 
     render() {
       return (
