1e741e6743b1af8e8573a0bdc327fe39967a2200
Bowie <lfranckx@thinkful.com>
2020-12-11 16:54:48 -0800

    fix EMOM and AMRAP

--- src/FormQuests/type-quest.js
+++ src/FormQuests/type-quest.js
@@ -10,10 +10,10 @@ export default class TypeQuest extends React.Component {
                 <h2 className="workout-question-3">What style of workout would you like?</h2>
                 <div className="workout-type answer-3">
                 
-                <input type="radio" id="emom" name="workoutTypeValue" value='emom' onClick={this.props.handleChange} />
+                <input type="radio" id="emom" name="workoutTypeValue" value='EMOM' onClick={this.props.handleChange} />
                 <label htmlFor="emom">EMOM (Every Minute On the Minute)</label>
                 
-                <input type="radio" id="amrap" name="workoutTypeValue" value='amrap' onClick={this.props.handleChange} />
+                <input type="radio" id="amrap" name="workoutTypeValue" value='AMRAP' onClick={this.props.handleChange} />
                 <label htmlFor="amrap">AMRAP (As Many Rounds As Possible)</label>
 
                 </div>
