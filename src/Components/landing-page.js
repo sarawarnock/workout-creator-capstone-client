f266618c53bdd2e9952a12821b2f8615459360ce
sarawarnock <sara@sarawarnock.com>
2020-12-12 16:59:30 -0700

    made reps even and took out the AMRAP parts

--- src/Components/landing-page.js
+++ src/Components/landing-page.js
@@ -5,15 +5,16 @@ export default class LandingPage extends React.Component {
   render() {
     return (
       <main className="main">
-        <h2>Create a metcon workout</h2>
+        <h2>Create An EMOM Style Workout</h2>
         <div className="landing-text">
-          <p>What is metcon? Metcon is short for metabolic conditioning a term that incorporates short bouts of higher-intensity training designed to increase metabolic demand and increase energy usage.</p>
-          <p>1. Create your own metabolic-conditioning workout</p>
-          <p>2. Choose what muscle groups you want to work, the duration of your workout, and a unique workout with instructions will be created.</p> 
-          <p>3. Create an account to save your workouts and keep track of your progress!</p>
+          <p>What is an EMOM? EMOM stands for 'Every Minute On the Minute' - a style of workout where you do a different exercise each minute (and repeat for a given number of minutes)! 
+              This style is a type of MetCon. MetCon is short for metabolic conditioning; a term that incorporates short bouts of higher-intensity training designed to increase metabolic demand and increase energy usage.</p>
+          <p>Create your own metabolic-conditioning workout!</p>
+          <p>Create an account to save your workouts and keep track of your progress</p>
+          <p>Choose what muscle groups you want to work and the duration of your workout, and a unique workout with instructions will be created.</p> 
         </div>
-        <Link to="/create-workout"
-          className="try-it-link link">Let's try it</Link>
+        {/* <Link to="/create-workout"
+          className="try-it-link link">Let's try it</Link> */}
         </main>
     );
   }
