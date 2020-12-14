e2aa52ebfd4fd3a3b26fe7e9bbf59de038c475e6
Bowie <lfranckx@thinkful.com>
2020-12-13 20:45:10 -0800

    edit landing page text / raise tablehead items to the top of container

--- src/Components/NavBars/render-headers.js
+++ src/Components/NavBars/render-headers.js
@@ -1,7 +1,11 @@
 const NavLinks = ({children}) => {
     return (
         <>
-            
+            <nav>
+                <Link className="home-link" to='/'>
+                <h1>MyMetcon</h1>
+                </Link>
+            </nav>
         </>
     )
 }
--- src/Components/landing-page.js
+++ src/Components/landing-page.js
@@ -7,11 +7,23 @@ export default class LandingPage extends React.Component {
       <main className="main">
         <h2>Create An EMOM Style Workout</h2>
         <div className="landing-text">
-          <p>What is an EMOM? EMOM stands for 'Every Minute On the Minute' - a style of workout where you do a different exercise each minute (and repeat for a given number of minutes)! 
-              This style is a type of MetCon. MetCon is short for metabolic conditioning; a term that incorporates short bouts of higher-intensity training designed to increase metabolic demand and increase energy usage.</p>
-          <p>Create your own metabolic-conditioning workout!</p>
-          <p>Create an account to save your workouts and keep track of your progress</p>
-          <p>Choose what muscle groups you want to work and the duration of your workout, and a unique workout with instructions will be created.</p> 
+          <p>What is an EMOM? EMOM stands for 'Every Minute On the Minute' - a style of workout 
+              where you do a different exercise each minute (and repeat for a given number of minutes)! 
+              This style is a type of MetCon. MetCon is short for metabolic conditioning; a term that 
+              incorporates short bouts of higher-intensity training designed to increase metabolic demand 
+              and increase energy usage.</p>
+            <ul>
+              <li>
+                <p>Sign up to create your own metabolic-conditioning workouts!</p>
+              </li>
+              <li>
+                <p>Save your workouts and keep track of your progress.</p>
+              </li>
+              <li>
+                <p>Select which muscle groups you would like to work and the duration of your workout, 
+                  and a unique workout with instructions will be created.</p> 
+              </li>
+            </ul>
         </div>
         {/* <Link to="/sign-up"
           className="try-it-link link">Let's try it</Link> */}
