47231f4fc65fa8f521d75863b431cf0d234b0ae0
Bowie <lfranckx@thinkful.com>
2020-12-28 13:11:39 -0800

    chnge server endpoint

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@
 // }
 
 export default {
-    API_ENDPOINT: "https://workout-creator-server.herokuapp.com/api",
-    // API_ENDPOINT: "http://localhost:8800/api",
+    // API_ENDPOINT: "https://workout-creator-server.herokuapp.com/api",
+    API_ENDPOINT: "http://localhost:8800/api",
     TOKEN_KEY: 'workout-client-auth-token',
 }
