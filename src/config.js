f2d146bff190098bfef2441950556665c9b60d8d
sarawarnock <sara@sarawarnock.com>
2021-01-06 10:43:52 -0700

    config

--- src/config.js
+++ src/config.js
@@ -11,8 +11,8 @@
 // }
 
 const config = {
-    API_ENDPOINT: "http://localhost:8800/api",
-    // API_ENDPOINT: "https://workout-creator-server.herokuapp.com/api",
+    //API_ENDPOINT: "http://localhost:8800/api",
+    API_ENDPOINT: "https://workout-creator-server.herokuapp.com/api",
     TOKEN_KEY: 'workout-client-auth-token',
 }
 
